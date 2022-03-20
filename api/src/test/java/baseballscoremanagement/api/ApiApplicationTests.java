5ebb81bc77c7baffa90acd79699cc4158cff1aed
Yoshiki Sato <narutodisney77@gmail.com>
2022-03-20 17:19:31 +0900

    spring initializrでAPIの初期設定ファイルを作成

---
+++ api/src/test/java/baseballscoremanagement/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package baseballscoremanagement.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
