ad654fcfa9ca5301944df1bca057c27a0c1861f6
Yoshiki Sato <narutodisney77@gmail.com>
2022-03-20 18:39:55 +0900

    サービスの実装クラスをとりあえず作成

---
+++ api/src/main/com/baseballscoremanagement/api/application/impl/TeamsServiceImpl.java
@@ -0,0 +1,12 @@
+package main.com.baseballscoremanagement.api.application.impl;
+
+import main.com.baseballscoremanagement.api.application.TeamsService;
+import main.com.baseballscoremanagement.api.domain.TeamSort;
+
+public class TeamsServiceImpl implements TeamsService {
+
+  @Override
+  public String getTeams(final TeamSort sort, final int start, final int results) {
+    return null;
+  }
+}
