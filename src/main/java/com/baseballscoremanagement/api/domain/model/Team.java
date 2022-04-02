116f8de92fbfa22c1eea449f891fc1b157306eac
Yoshiki Sato <narutodisney77@gmail.com>
2022-04-02 21:40:57 +0900

    TeamのGetter修正

--- src/main/java/com/baseballscoremanagement/api/domain/model/Team.java
+++ src/main/java/com/baseballscoremanagement/api/domain/model/Team.java
@@ -3,9 +3,11 @@ package com.baseballscoremanagement.api.domain.model;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 @AllArgsConstructor
 @EqualsAndHashCode
+@Getter
 public class Team {
   private int id;
   private final String name;
