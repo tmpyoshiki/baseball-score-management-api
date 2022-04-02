0c654ebd88b66cbaacf83c90473850c2fe67ff4a
Yoshiki Sato <narutodisney77@gmail.com>
2022-04-02 18:52:33 +0900

    repositoryテスト作成

---
+++ src/test/java/com/baseballscoremanagement/api/infrastracture/repository/TeamsRepositoryImplTest.java
@@ -0,0 +1,42 @@
+package com.baseballscoremanagement.api.infrastracture.repository;
+
+import com.baseballscoremanagement.api.domain.model.Team;
+import com.baseballscoremanagement.api.domain.sort.TeamSort;
+import com.baseballscoremanagement.api.infrastracture.library.TeamsMySqlLibrary;
+import com.baseballscoremanagement.api.infrastracture.response.TeamResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+class TeamsRepositoryImplTest {
+
+  @Mock
+  private TeamsMySqlLibrary teamsMySqlLibrary;
+
+  @InjectMocks
+  private TeamsRepositoryImpl teamsRepositoryImpl;
+
+  @Nested
+  class getTeamList {
+    @Test
+    void 取得した結果を正常に返すことができること() {
+      final var expectTeam = new Team(1, "テストチーム");
+      final var teamResponse = Flux.just(new TeamResponse(1, "テストチーム"));
+      Mockito.doReturn(teamResponse).when(teamsMySqlLibrary).findTeams(1, 5);
+      final var actualTeamList = teamsRepositoryImpl.getTeamList(TeamSort.DESC_GAMES, 1, 5);
+
+      StepVerifier.create(actualTeamList).assertNext(team -> {
+        Assertions.assertEquals(expectTeam, team);
+      });
+      Mockito.verify(teamsMySqlLibrary, Mockito.times(1)).findTeams(1, 5);
+    }
+  }
+}
