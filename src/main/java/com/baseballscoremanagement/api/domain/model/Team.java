28d078e3d1b3170810eba4865c361a8970896723
Yoshiki Sato <narutodisney77@gmail.com>
2022-04-02 17:37:30 +0900

    チームのModel作成

---
+++ src/main/java/com/baseballscoremanagement/api/domain/model/Team.java
@@ -0,0 +1,10 @@
+package com.baseballscoremanagement.api.domain.model;
+
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Team {
+  private int id;
+  private final String name;
+}
