8633fe0669fc7dce02e7165485516fea5f7a0bcb
Yoshiki Sato <narutodisney77@gmail.com>
2022-04-02 18:54:08 +0900

    テスト修正によるentityを修正

--- src/main/java/com/baseballscoremanagement/api/domain/model/Team.java
+++ src/main/java/com/baseballscoremanagement/api/domain/model/Team.java
@@ -2,8 +2,10 @@ package com.baseballscoremanagement.api.domain.model;
 
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Team {
   private int id;
   private final String name;
--- src/main/java/com/baseballscoremanagement/api/infrastracture/response/TeamResponse.java
+++ src/main/java/com/baseballscoremanagement/api/infrastracture/response/TeamResponse.java
@@ -1,9 +1,11 @@
 package com.baseballscoremanagement.api.infrastracture.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 
 @Getter
+@AllArgsConstructor
 public class TeamResponse {
   @Id
   private int id;
