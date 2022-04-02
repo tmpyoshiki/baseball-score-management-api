b08f43345593ef5147d39cdaed40695c0ecd36b6
Yoshiki Sato <narutodisney77@gmail.com>
2022-04-02 17:38:41 +0900

    Controller作成

--- src/main/java/com/baseballscoremanagement/api/interfaces/v1/TeamsController.java
+++ src/main/java/com/baseballscoremanagement/api/interfaces/v1/TeamsController.java
@@ -1,11 +1,14 @@
 package com.baseballscoremanagement.api.interfaces.v1;
 
 import com.baseballscoremanagement.api.application.TeamsService;
+import com.baseballscoremanagement.api.domain.model.Team;
 import com.baseballscoremanagement.api.domain.sort.TeamSort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1")
@@ -25,14 +28,14 @@ public class TeamsController {
    * @param sort　ソート順
    * @param start 取得開始位置
    * @param results 取得数
-   * @return TODO: とりあえずstringにしているがあとでちゃんと定義
+   * @return チーム一覧
    */
   @GetMapping("/teams")
-  public String getTeams(
+  public List<Team> getTeamList(
     @RequestParam(value = "sort", required = false, defaultValue = "desc_games") final TeamSort sort,
     @RequestParam(value = "start", required = false, defaultValue = "0") final int start,
     @RequestParam(value = "results", required = false, defaultValue = "3") final int results
   ){
-    return this.teamsService.getTeams(sort, start, results);
+    return this.teamsService.getTeamList(sort, start, results);
   }
 }
