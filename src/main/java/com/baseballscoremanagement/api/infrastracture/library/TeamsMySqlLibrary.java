5db5686ba84ff811ecf8316865b15427467e15bc
Yoshiki Sato <narutodisney77@gmail.com>
2022-04-02 18:17:55 +0900

    アノテーション忘れ修正

--- src/main/java/com/baseballscoremanagement/api/infrastracture/library/TeamsMySqlLibrary.java
+++ src/main/java/com/baseballscoremanagement/api/infrastracture/library/TeamsMySqlLibrary.java
@@ -3,8 +3,10 @@ package com.baseballscoremanagement.api.infrastracture.library;
 import com.baseballscoremanagement.api.infrastracture.response.TeamResponse;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+@Component
 public interface TeamsMySqlLibrary extends ReactiveCrudRepository<TeamResponse, Integer> {
 
   @Query("SELECT * FROM TEAMS LIMIT :results OFFSET :start")
