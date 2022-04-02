5176ee566012abc4d8f4ff65aa327282c474d892
Yoshiki Sato <narutodisney77@gmail.com>
2022-04-02 21:41:49 +0900

    controllerで間違っていた箇所修正

--- src/main/java/com/baseballscoremanagement/api/interfaces/v1/TeamsController.java
+++ src/main/java/com/baseballscoremanagement/api/interfaces/v1/TeamsController.java
@@ -1,14 +1,12 @@
 package com.baseballscoremanagement.api.interfaces.v1;
 
 import com.baseballscoremanagement.api.application.TeamsService;
-import com.baseballscoremanagement.api.domain.model.Team;
 import com.baseballscoremanagement.api.domain.sort.TeamSort;
+import com.baseballscoremanagement.api.interfaces.v1.response.TeamListResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("v1")
@@ -31,11 +29,13 @@ public class TeamsController {
    * @return チーム一覧
    */
   @GetMapping("/teams")
-  public List<Team> getTeamList(
-    @RequestParam(value = "sort", required = false, defaultValue = "desc_games") final TeamSort sort,
-    @RequestParam(value = "start", required = false, defaultValue = "1") final int start,
-    @RequestParam(value = "results", required = false, defaultValue = "3") final int results
+  public TeamListResponse getTeamList(
+    @RequestParam(value = "sort", defaultValue = "DESC_GAMES") final String sort,
+    @RequestParam(value = "start", defaultValue = "1") final int start,
+    @RequestParam(value = "results", defaultValue = "3") final int results
   ){
-    return this.teamsService.getTeamList(sort, start, results);
+    final var teamSort = TeamSort.valueOf(sort);
+    final var teamList = this.teamsService.getTeamList(teamSort, start, results);
+    return new TeamListResponse(teamList);
   }
 }
