aff8f9bcd10771a9908843df1df8c98f3a2d8d0b
Yoshiki Sato <narutodisney77@gmail.com>
2022-04-02 18:17:00 +0900

    startを1始まりに修正

--- src/main/java/com/baseballscoremanagement/api/interfaces/v1/TeamsController.java
+++ src/main/java/com/baseballscoremanagement/api/interfaces/v1/TeamsController.java
@@ -1,6 +1,6 @@
 package com.baseballscoremanagement.api.interfaces.v1;
 
-import com.baseballscoremanagement.api.application.TeamsService;
+import com.baseballscoremanagement.api.application.impl.TeamsService;
 import com.baseballscoremanagement.api.domain.model.Team;
 import com.baseballscoremanagement.api.domain.sort.TeamSort;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,7 +33,7 @@ public class TeamsController {
   @GetMapping("/teams")
   public List<Team> getTeamList(
     @RequestParam(value = "sort", required = false, defaultValue = "desc_games") final TeamSort sort,
-    @RequestParam(value = "start", required = false, defaultValue = "0") final int start,
+    @RequestParam(value = "start", required = false, defaultValue = "1") final int start,
     @RequestParam(value = "results", required = false, defaultValue = "3") final int results
   ){
     return this.teamsService.getTeamList(sort, start, results);
